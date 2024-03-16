b293ae71159ae92e99399569495683f4bb3d310b
Mohamed Abdelaal <69994317+Mohamed-004@users.noreply.github.com>
2024-03-16 19:13:22 -0400

    made the apppy fo the heroku app by removing the local server

--- app.py
+++ app.py
@@ -19,5 +19,5 @@ def featured_firms():
 def tos():
     return render_template('terms-of-service.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
