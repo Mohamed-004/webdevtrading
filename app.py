4329a85a27d886c7f0d544f6c8b8a1761a1ebdf2
Mohamed Abdelaal <69994317+Mohamed-004@users.noreply.github.com>
2024-03-16 19:22:31 -0400

    fixed procfile

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
+app.config['DEBUG'] = True
 
 @app.route('/')
 def home():
@@ -19,5 +19,5 @@ def featured_firms():
 def tos():
     return render_template('terms-of-service.html')
 
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
