82968539be1cba82eb0121421bc9aa15ab82996a
Mohamed Abdelaal <69994317+Mohamed-004@users.noreply.github.com>
2024-03-21 01:47:20 -0400

    added dei, bespoke, and kortana

--- app.py
+++ app.py
@@ -19,9 +19,13 @@ def featured_firms():
 def tos():
     return render_template('terms-of-service.html')
 
-@app.route('/reports/tft-10a')
-def tft_10():
-    return render_template('report_1_mock.html')
+# @app.route('/reports/tft-10a')
+# def tft_10():
+#     return render_template('report_1_mock.html')
+
+@app.route('/reports/dei-20a')
+def dei_20a():
+    return render_template('dei_case_20a.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
