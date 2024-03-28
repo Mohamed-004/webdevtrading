2c376290dec28f48b4e7efdce602d3533e334f4f
Mohamed Abdelaal <69994317+Mohamed-004@users.noreply.github.com>
2024-03-28 05:03:10 -0400

    fixed the robots.txt

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+@app.route('/robots.txt')
+def static_from_root():
+    return send_from_directory(app.static_folder, 'robots.txt')
 
 @app.route('/')
 def home():
