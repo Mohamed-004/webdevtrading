b0e2bd523dea74a2ffb4ea2e3104ec78563c8c5b
Mohamed Abdelaal <69994317+Mohamed-004@users.noreply.github.com>
2024-04-10 14:42:54 -0400

    login fixed and made dashboard

--- app.py
+++ app.py
@@ -1,7 +1,95 @@
-from flask import Flask, render_template, send_from_directory
+from authlib.integrations.flask_client import OAuth
+import firebase_admin
+from firebase_admin import credentials, auth, db, firestore
+from flask import Flask, render_template, send_from_directory, jsonify, request, url_for, redirect, session
+from urllib.parse import quote_plus, urlencode
+import requests
+import secrets
 
 app = Flask(__name__)
+app.secret_key = '1799d3118e5549432de9b191ba8003c8826585499ebba1974de60d36c8c2c2e6'
 app.config['DEBUG'] = True
+
+cred = credentials.Certificate('static/prop-patrol24-firebase-adminsdk-i50kg-5fe571bc7b.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+oauth = OAuth(app)
+conf_url = 'https://dev-ct0rwl0778orlwvk.us.auth0.com/.well-known/openid-configuration'
+oauth.register(
+    name='proppatrol',
+    client_id='qA5AwBA91VxeHowQQu6MDKOBYHWbWbmx',
+    client_secret='7FckIXvKm00XxFch0RDB9iGiATPnKZ_RqnL83Um_BILE4_gQyL7fYLi7MfW431hn',
+    server_metadata_url=conf_url,
+    client_kwargs={'scope': 'openid profile email'},
+)
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.proppatrol.authorize_access_token()
+    user_info = {
+    "email": token["userinfo"]["email"],
+    "email_verified": token["userinfo"]["email_verified"]
+}
+
+# Store the extracted user information in the session
+    session["user"] = user_info
+    user = oauth.proppatrol.parse_id_token(token,nonce=session.get("nonce"))
+ 
+    try:
+        firebase_user = auth.get_user_by_email(user['email'])
+    except auth.UserNotFoundError:
+        # User not found in Firebase Authentication, create new user
+        user_ref = db.collection('users').document(user['email'])
+        user_ref.set({
+        'uid': user['sub'],  # Assuming sub field contains UID
+        'name': user.get('name', ''),
+        'email': user['email'],
+        # Add other user information as needed
+    })
+    
+    return redirect(url_for('dashboard'))
+
+
+
+@app.route("/login")
+def login():
+    return oauth.proppatrol.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True),
+    )
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://dev-ct0rwl0778orlwvk.us.auth0.com/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": 'qA5AwBA91VxeHowQQu6MDKOBYHWbWbmx',
+            },
+            quote_via=quote_plus,
+        )
+    )
+
+@app.route("/dashboard")
+def dashboard():
+    # Check if user is authenticated
+    try:
+        user_info = session.get("user")
+        user_email = user_info['email']
+        user_data= db.collection("users").where(field_path="email", op_string="==", value=user_email).get()
+    except:
+        pass
+    # print(user_data + " the value for user data came")
+    if not user_info or not user_data:
+        # Redirect to login page if user is not authenticated or email is not verified
+        return redirect(url_for("login"))
+    # firebase_user_data = list(user_data.values())[0]
+    
+    return render_template("dashboard.html", session=user_info)
+
+
 
 @app.route('/robots.txt')
 def static_from_root():
@@ -10,6 +98,8 @@ def static_from_root():
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
 
 @app.route('/report-payout')
 def report_payout():
@@ -23,9 +113,7 @@ def featured_firms():
 def tos():
     return render_template('terms-of-service.html')
 
-# @app.route('/reports/tft-10a')
-# def tft_10():
-#     return render_template('report_1_mock.html')
+
 
 @app.route('/reports/dei-20a')
 def dei_20a():
